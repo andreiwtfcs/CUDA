5b4e642e7926545731b83f15850feadba05892f4
andreiwtfcs <velceaandrei@gmail.com>
2018-05-15 11:05:58 +0300

    Update

--- Roy_Floyd/kernel.cu
+++ Roy_Floyd/kernel.cu
@@ -7,30 +7,17 @@
 
 using namespace std;
 
-__global__ void RoyFloyd(int matrixGraph[NR][NR])
-{
-
-	int i = threadIdx.x;
-	int j = threadIdx.y;
-	for (int k = 1; k <= NR; k++)
-	{
-		if (matrixGraph[i][k] + matrixGraph[k][j] < matrixGraph[i][j]) {
-			matrixGraph[i][j] = matrixGraph[i][k] + matrixGraph[k][j];
-		}
-	}
-}
-
-int h_matrixGraph[][] = {
-	{ 0, 2, INF, 10, INF },
-{ 2, 0, 3, INF, INF },
-{ INF, 3, 0, 1, 8 },
-{ 10, INF, 1, 0, INF },
-{ INF, INF, 8, INF, 0 }
-};
-
 int main()
 {
 	int *d_matrix;
+	
+	int h_matrixGraph[][] = {
+		{ 0, 2, INF, 10, INF },
+		{ 2, 0, 3, INF, INF },
+		{ INF, 3, 0, 1, 8 },
+		{ 10, INF, 1, 0, INF },
+		{ INF, INF, 8, INF, 0 }
+	};
 
 	cudaMalloc(&d_matrix, NR*NR * sizeof(int)); //alocam memorie liniara
 
@@ -65,4 +52,17 @@ int main()
 	system("pause");
 	return 0;
 
+}
+
+__global__ void RoyFloyd(int matrixGraph[NR][NR])
+{
+
+	int i = threadIdx.x;
+	int j = threadIdx.y;
+	for (int k = 1; k <= NR; k++)
+	{
+		if (matrixGraph[i][k] + matrixGraph[k][j] < matrixGraph[i][j]) {
+			matrixGraph[i][j] = matrixGraph[i][k] + matrixGraph[k][j];
+		}
+	}
 }
